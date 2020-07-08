0371863aed64c119568adc93b4906f995e18a839
Ghevi <ghevi96@gmail.com>
2020-07-08 16:14:44 +0200

    Added arrow functions

--- app.js
+++ app.js
@@ -1,1 +1,102 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
+const RESULT_DRAW = "DRAW";
+const RESULT_PLAYER_WINS = "PLAYER_WINS";
+const RESULT_COMPUTER_WINS = "COMPUTER_WINS";
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+const getComputerChoice = () => {
+  const randomValue = Math.random();
+  if (randomValue < 0.34) {
+    return ROCK;
+  } else if (randomValue < 0.67) {
+    return PAPER;
+  } else {
+    return SCISSORS;
+  }
+};
+
+const getWinner = (cChoice, pChoice) =>
+  cChoice === pChoice
+    ? RESULT_DRAW
+    : (cChoice === ROCK && pChoice === PAPER) ||
+      (cChoice === PAPER && pChoice === SCISSORS) ||
+      (cChoice === SCISSORS && pChoice === ROCK)
+    ? RESULT_PLAYER_WINS
+    : RESULT_COMPUTER_WINS;
+
+// if (cChoice === pChoice) {
+//   return RESULT_DRAW;
+// } else if (
+//   (cChoice === ROCK && pChoice === PAPER) ||
+//   (cChoice === PAPER && pChoice === SCISSORS) ||
+//   (cChoice === SCISSORS && pChoice === ROCK)
+// ) {
+//   return RESULT_PLAYER_WINS;
+// } else {
+//   return RESULT_COMPUTER_WINS;
+// }
+// }
+
+// Be carefull of memory leaks! In this case just 1 ev.listener so no problem
+startGameBtn.addEventListener("click", () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting...");
+  const playerChoice = getPlayerChoice();
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(computerChoice, playerChoice);
+  console.log(winner);
+});
+
+/* *****************************************************************************************************
+const add = a, b => a + b;
+
+const add2 = function(a, b) {
+  return a + b;
+}
+
+// console.log(typeof startGame); // calling it will print the type of what is returned, without the () we see the type "function"
+// console.dir(startGame);
+
+// const start = function () {
+//   console.log("Game is starting...");
+// };
+
+// startGameBtn.addEventListener("click", start);
+
+// startGameBtn.addEventListener("click", function startGame(){ // Be carefull of memory leaks! In this case just 1 ev.listener so no problem
+//   console.log("Game is loading...", age); // naming the anonymous function will make it easy to spot if it produces errors.
+// });
+
+// METHODS:
+
+// const person = {
+//   name: 'Max', // property
+//   greet: function greet() { // a function inside an object is a method
+//     console.log('Hi max!');
+//   }
+// }
+
+// person.greet();
+
+*/
