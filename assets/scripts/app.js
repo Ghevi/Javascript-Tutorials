0179e36d60e62e006d5d8646ef4d31f1a684d076
Ghevi <ghevi96@gmail.com>
2020-07-17 18:36:06 +0200

    modified:   assets/scripts/app.js

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,70 @@
-const modal = document.getElementById("add-modal");
-const button = document.querySelector("header").lastChild;
+//const addMovieModal = document.querySelector("#add-modal");
+//const addMovieModal = document.body.children[1];
+const addMovieModal = document.getElementById("add-modal"); // use this
+// const startAddMovieButton = document.querySelector("header").lastElementChild; // if we change the code, it wont be the last child anymore
+const startAddMovieButton = document.querySelector("header button");
+// const backdrop = document.body.firstElementChild;
+const backdrop = document.getElementById("backdrop");
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+//const inputs = addMovieModal.getElementsByTagName('input');
+const userInputs = addMovieModal.querySelectorAll("input");
 
-button.addEventListener("click", () => {
-  modal.style="visible";
-});
+const movies = [];
+
+const toggleBackdrop = () => {
+  backdrop.classList.toggle("visible");
+};
+
+const toggleMovieModel = () => {
+  addMovieModal.classList.toggle("visible");
+  toggleBackdrop();
+};
+
+const clearMovieInput = () => {
+  for (const usrInputs of userInputs) {
+    usrInputs.value = "";
+  }
+};
+
+const cancelAddMovieHandler = () => {
+  toggleMovieModel();
+  clearMovieInput();
+};
+
+const addMovieHandler = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert("Please enter valid values (rating between 1 and 5).");
+    return;
+  }
+
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  toggleMovieModel();
+  clearMovieInput();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModel();
+};
+
+startAddMovieButton.addEventListener("click", toggleMovieModel);
+backdrop.addEventListener("click", backdropClickHandler);
+cancelAddMovieButton.addEventListener("click", cancelAddMovieHandler);
+confirmAddMovieButton.addEventListener("click", addMovieHandler);
