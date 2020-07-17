93ba0e6cc7f8aaefd271d0eb5606a2ea3b698024
Ghevi <ghevi96@gmail.com>
2020-07-17 17:51:38 +0200

    new file:   assets/scripts/app.js 	new file:   assets/styles/app.css 	new file:   index.html

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+const modal = document.getElementById("add-modal");
+const button = document.querySelector("header").lastChild;
+
+button.addEventListener("click", () => {
+  modal.style="visible";
+});
