c8cd70d0427748e45f70f876849c9a1eb72b78f2
Ghevi <ghevi96@gmail.com>
2020-07-21 00:40:22 +0200

    Added Classes for shopping cart

--- OOP/assets/scripts/app.js
+++ OOP/assets/scripts/app.js
@@ -12,8 +12,59 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
+    return cartEl;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+        <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this)); // without bind(this) we get undefined
+    return prodEl;
+  }
+
+  addToCart() {
+    // console.log("Adding product to cart...");
+    // console.log(this.product);
+    App.addProductToCart(this.product);
+  }
+}
+
+class ProductList {
+  products = [
     new Product(
       "A Pillow",
       "https://www.downetc.com/media/catalog/product/cache/4c0ee9c259e8813fe6f171ce41105c1f/g/o/goose_down_pillow_rhapsody.jpg",
@@ -22,33 +73,55 @@ const productList = {
     ),
     new Product(
       "A Carpet",
-      "https://lh3.googleusercontent.com/proxy/yyBF6zNhoS1wmw0HFbbatqPmSsnwcc9hn5ZRjNNI1RuTpd4pRDo-e8-hPLzo1r5RrYFV2rI73XdKaYP-_C3ZygW6o4rdO7i71WTB6WHio31Nt3ehJVH0ntTf",
+      "https://www.fatboy.com/assets/image/000/000/124/fatboy-non-flying-carpets-paprika.png",
       89.99,
       "A carpet that will make you fly"
     ),
-  ],
+  ];
+
+  constructor() {}
+
   render() {
-    const renderHook = document.getElementById("app");
+    // const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}">
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
-  },
-};
+    return prodList;
+  }
+}
 
-productList.render();
+class Shop {
+  // cart; // can be omitted because this.cart automatically create this property 
+
+  render() {
+    const renderHook = document.getElementById("app");
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
