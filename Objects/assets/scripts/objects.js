aa88fe3a43965fd6c67512e8773417e11e65f031
Ghevi <ghevi96@gmail.com>
2020-07-20 19:30:16 +0200

    Finished adding and searching

--- Objects/assets/scripts/objects.js
+++ Objects/assets/scripts/objects.js
@@ -22,31 +22,37 @@ const renderMovies = (filter = "") => {
     const movieEl = document.createElement("li");
 
     // These two are the same, check info exist in movie
-    if(!('info' in movie)) {
+    if (!("info" in movie)) {
       // if needed
     }
-    if(movie.info === undefined) {
+    if (movie.info === undefined) {
       // if needed
     }
 
     const { info, ...otherProps } = movie; // object destructoring + rest parameter
     console.log(otherProps);
-    const { title: movieTitle } = info;
-    let text = movieTitle + " - ";
+    let { getFormattedTitle } = movie;
+    let text = getFormattedTitle.apply(movie) + " - "; // apply uses an array apply(movie, [])
     for (const key in info) {
       // look below *
-      if (key !== "title") {
-        // text = text + `${key}: ${movie.info[key]}`;
+      if (key !== "title" && key !== '_title') {
         text = text + `${key}: ${info[key]}`;
       }
     }
     movieEl.textContent = text;
     movieList.append(movieEl);
 
+    // const { title: movieTitle } = info;
+    // let text = movie.getFormattedTitle() + " - "; // the this below in the obj refer to movie
+    // getFormattedTitle = getFormattedTitle.bind(movie); // this will refer to movie and not window obj
+    // let text = getFormattedTitle.call(movie, , ,) + " - "; // call multiple arguments , , ,
+
     // movieEl.textContent = movie.info.title;
     // let text = movie.info.title + " - ";
     // let text = info.title + " - ";
     // for (const key in movie.info) {
+    //   text = text + `${key}: ${movie.info[key]}`;
+    // }
   });
 };
 
@@ -56,7 +62,7 @@ const addMovieHandler = () => {
   const extraValue = document.getElementById("extra-value").value;
 
   if (
-    title.trim() === "" ||
+    // title.trim() === "" ||
     extraName.trim() === "" ||
     extraValue.trim() === ""
   ) {
@@ -65,17 +71,40 @@ const addMovieHandler = () => {
 
   const newMovie = {
     info: {
-      title, // title: title
+      // title, // title: title
+      set title(val) {
+        // setter, without this the property is read only
+        if (val.trim() === "") {
+          this._title = "DEFAULT";
+          return;
+        }
+        this._title = val;
+      },
+      get title() {
+        // getter
+        return this._title.toUpperCase();
+      },
       [extraName]: extraValue,
     },
     id: Math.random().toString(),
+    // it s the shorter of getFormattedTitle: function ()
+    // dont user arrow function for this one because it doesnt bind this to anything
+    getFormattedTitle() {
+      console.log(this);
+      return this.info.title.toUpperCase(); // this refer to the thing that access it
+    },
   };
+
+  newMovie.info.title = title;
+  console.log(newMovie.info.title);
 
   movies.push(newMovie);
   renderMovies();
 };
 
+// const searchMovieHandler = function () {
 const searchMovieHandler = () => {
+  console.log(this); // searchBtn with function(), Window if we use => because arrow function dont know about this
   const filterTerm = document.getElementById("filter-title").value;
   renderMovies(filterTerm);
 };
