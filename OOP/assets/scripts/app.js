55c32b1543cd6cf8c9b5f83af427043de59693b0
Ghevi <ghevi96@gmail.com>
2020-07-20 20:33:22 +0200

    Added Product class and removed product literals

--- OOP/assets/scripts/app.js
+++ OOP/assets/scripts/app.js
@@ -1,19 +1,31 @@
+class Product {
+  title = "DEFAULT";
+  imageUrl;
+  price;
+  description;
+
+  constructor(title, image, price, desc) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
+  }
+}
+
 const productList = {
   products: [
-    {
-      title: "A Pillow",
-      imageUrl:
-        "https://www.downetc.com/media/catalog/product/cache/4c0ee9c259e8813fe6f171ce41105c1f/g/o/goose_down_pillow_rhapsody.jpg",
-      price: 19.99,
-      description: "A soft pillow",
-    },
-    {
-      title: "A Carpet",
-      imageUrl:
-        "https://lh3.googleusercontent.com/proxy/yyBF6zNhoS1wmw0HFbbatqPmSsnwcc9hn5ZRjNNI1RuTpd4pRDo-e8-hPLzo1r5RrYFV2rI73XdKaYP-_C3ZygW6o4rdO7i71WTB6WHio31Nt3ehJVH0ntTf",
-      price: 89.99,
-      description: "A carpet that will make you fly",
-    },
+    new Product(
+      "A Pillow",
+      "https://www.downetc.com/media/catalog/product/cache/4c0ee9c259e8813fe6f171ce41105c1f/g/o/goose_down_pillow_rhapsody.jpg",
+      19.99,
+      "A soft pillow"
+    ),
+    new Product(
+      "A Carpet",
+      "https://lh3.googleusercontent.com/proxy/yyBF6zNhoS1wmw0HFbbatqPmSsnwcc9hn5ZRjNNI1RuTpd4pRDo-e8-hPLzo1r5RrYFV2rI73XdKaYP-_C3ZygW6o4rdO7i71WTB6WHio31Nt3ehJVH0ntTf",
+      89.99,
+      "A carpet that will make you fly"
+    ),
   ],
   render() {
     const renderHook = document.getElementById("app");
