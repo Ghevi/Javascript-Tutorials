e9493427e2f65f0647f85873424b56f0b00db4de
Ghevi <ghevi96@gmail.com>
2020-08-05 00:32:32 +0200

    Finished advanced functions

--- app.js
+++ app.js
@@ -67,7 +67,6 @@ console.log(calculateVatAmount(200));
 // so you need to store its references that is returned by the factory func createTaxCalculator 2 times
 // each for calculateVatAmount and calculateIncomeTaxAmount
 
-
 // CLOSURES
 
 // Every function is a closure! For interviews
@@ -77,3 +76,145 @@ console.log(calculateVatAmount(200));
 
 // Each lexical environment is created when the function is called/runs. So everytime we call
 // createTaxCalculator(tax), calculateTax(amount) locks the tax that is passed to the outer function
+
+// Exercises for closures
+
+// let username = 'Max';
+
+// function greetUser() {
+//   console.log('Hi ' + username); // When created here, the function just locks the pointer to username, not the value
+// }
+
+// username = 'Manuel';
+
+// greetUser();
+
+// *********************************************************************************************
+
+// let username = 'Max';
+
+// function greetUser() {
+//   let name = username; // Again hi manuel, the line reaches out to the outer environment when is run
+//   console.log('Hi ' + name); // Again hi manuel, the line reaches out to the outer environment when is run
+// }
+
+// username = 'Manuel';
+
+// greetUser();
+
+// *********************************************************************************************
+
+// let username = 'Max';
+
+// function greetUser() {
+//   let name = 'Anna'; // Variable shadowing, only if it cant find a name in this inner enviroment it goes outside
+//   console.log('Hi ' + name); // Hi Anna because we store 'Anna' in name not the reference to username or name
+// }
+
+// let name = 'Maximilian';
+
+// username = 'Manuel';
+
+// greetUser();
+
+// *********************************************************************************************
+
+let username = 'Max';
+
+function greetUser() {
+  // let name = 'Anna'; // Variable shadowing, only if it cant find a name in this inner enviroment it goes outside
+  console.log('Hi ' + name); // No is hi maximilian because it cant find name here.
+}
+
+let name = 'Maximilian';
+
+username = 'Manuel';
+
+greetUser();
+
+// RECURSION
+
+// function powerOf(x, n) {
+//   let result = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     result *= x;
+//   }
+
+//   return result;
+// }
+
+// console.log(powerOf(2, 3)); // 2 * 2 * 2
+
+// this is a recursive function, every time it reduces n because we have to multiply x * x only 3 times.
+// function powerOf(x, n) {
+//   if (n === 1) {
+//     return x;
+//   }
+//   return x * powerOf(x, n - 1);
+// }
+
+// console.log(powerOf(2, 3)); // 2 * 2 * 2
+
+// Even shorter way
+function powerOf(x, n) {
+  return n === 1 ? x : x * powerOf(x, n - 1);
+}
+
+console.log(powerOf(2, 3)); // 2 * 2 * 2
+
+// Practical use, this or a folder structure
+
+const myself = {
+  name: 'Max',
+  friends: [
+    {
+      name: 'Manuel',
+      friends: [
+        {
+          name: 'Chris',
+          //could   friends: etc etc
+          friends: [
+            {
+              name: 'Hari',
+            },
+            {
+              name: 'Emilia',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'Julia',
+      // etc etc
+    },
+  ],
+};
+
+// This is not feasable because we would need as many for loop as nesting levels at best, and at
+// worst if a friend doesnt have friends, friends will be undefined
+// function printFriendNames(person) {
+//   for(const friends of person.friends) {
+//     for(const friendsFriends of friends.friends){
+//       for(){}
+//     }
+//   }
+// }
+
+function getFriendNames(person) {
+  const collectedNames = [];
+
+  if (!person.friends) {
+    return [];
+  }
+
+  for (const friend of person.friends) {
+    collectedNames.push(friend.name);
+    collectedNames.push(...getFriendNames(friend));
+  }
+
+  return collectedNames;
+}
+
+console.log(getFriendNames(myself)); // ["Manuel", "Chris", "Hari", "Emilia", "Julia"]
