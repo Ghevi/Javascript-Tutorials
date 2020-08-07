8f0e8737f50510e4b3005e6005b1e33170cdef0f
Ghevi <ghevi96@gmail.com>
2020-08-08 01:59:06 +0200

    Finished http with fetch api

--- HttpRequests/assets/scripts/app.js
+++ HttpRequests/assets/scripts/app.js
@@ -1,45 +1,181 @@
-// HTTP REQUESTS
+// // GET POST REQUESTS
+
+// const listElement = document.querySelector('.posts');
+// const postTemplate = document.getElementById('single-post');
+// const form = document.querySelector('#new-post form');
+// const fetchButton = document.querySelector('#available-posts button');
+// const postList = document.querySelector('ul');
+
+// function sendHttpRequest(method, url, data) {
+//   const promise = new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.setRequestHeader('Content-Type', 'application/json' );
+
+//     // xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+//     xhr.open(method, url);
+
+//     // xhr.addEventListener(); Older broswer dont support it
+
+//     xhr.responseType = 'json'; // Automatic parse, not always possible tho*
+
+//     xhr.onload = () => {
+//       if (xhr.status >= 200 && xhr.stats < 300) {
+//         resolve(xhr.response);
+//       } else {
+//         xhr.response;
+//         reject(new Error('Something went wrong!'));
+//       }
+//       // console.log(xhr.response);
+//       // json to js = parse. js to json = stringify
+//       // const listOfPosts = JSON.parse(xhr.response); //*
+//     };
+
+//     // This is for network error, not server error like a 404 because
+//     // 404 is still a valid response, even if is not found.
+//     // So you must check on the onload above
+//     xhr.onerror = function () {
+//       reject(new Error('Failed to send request!')); // only works if fail to send request
+//       // console.log(xhr.response); // doesnt work for server errors
+//       // console.log(xhr.status);  // doesnt work for server errors
+//     };
+
+//     xhr.send(JSON.stringify(data));
+//   });
+//   return promise;
+// }
+
+// async function fetchPosts() {
+//   try {
+//     const responseData = await sendHttpRequest(
+//       'GET',
+//       'https://jsonplaceholder.typicode.com/pos'
+//     );
+
+//     const listOfPosts = responseData;
+//     for (const post of listOfPosts) {
+//       const postEl = document.importNode(postTemplate.content, true);
+//       postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//       postEl.querySelector('p').textContent = post.body;
+//       postEl.querySelector('li').id = post.id;
+//       listElement.append(postEl);
+//     }
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// }
+
+// async function createPost(title, content) {
+//   const userId = Math.random();
+//   const post = {
+//     title: title,
+//     body: content,
+//     userId: userId,
+//   };
+
+//   sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+// }
+
+// fetchButton.addEventListener('click', fetchPosts); // could also use () => {fetchposts();}
+
+// form.addEventListener('submit', (event) => {
+//   event.preventDefault(); // avoid broswer submitting the form
+//   const enteredTitle = event.currentTarget.querySelector('#title').value;
+//   const enteredContent = event.currentTarget.querySelector('#content').value;
+
+//   createPost(enteredTitle, enteredContent);
+// });
+
+// /* Without async await
+// function fetchPosts() {
+//   sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
+//     (responseData) => {
+//       const listOfPosts = xhr.response;
+//       for (const post of listOfPosts) {
+//         const postEl = document.importNode(postTemplate.content, true);
+//         postEl.querySelector('h2').textContent = post.title.toUpperCase();
+//         postEl.querySelector('p').textContent = post.body;
+//         listElement.append(postEl);
+//       }
+//     }
+//   );
+// */
+
+// // DELETE REQUEST
+
+// postList.addEventListener('click', (event) => {
+//   if (event.target.tagName === 'BUTTON') {
+//     const postId = event.target.closest('li').id;
+//     sendHttpRequest(
+//       'DELETE',
+//       `https://jsonplaceholder.typicode.com/posts/${postId}`
+//     );
+//   }
+// });
+
+// **************************************************************************
+
+// Modern alternative to XML, the fetch API
+
+// It is already a promise based api
+
+// GET POST REQUESTS
 
 const listElement = document.querySelector('.posts');
 const postTemplate = document.getElementById('single-post');
 const form = document.querySelector('#new-post form');
 const fetchButton = document.querySelector('#available-posts button');
+const postList = document.querySelector('ul');
 
+// fetch return a promise, get request, but it s a stream not parsed as js obj
+// so we need to use .json() on the response. (snapshot and then parse)
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
-
-    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-    xhr.open(method, url);
-
-    // xhr.addEventListener(); Older broswer dont support it
-
-    xhr.responseType = 'json'; // Automatic parse, not always possible tho*
-
-    xhr.onload = () => {
-      resolve(xhr.response);
-      // console.log(xhr.response);
-      // json to js = parse. js to json = stringify
-      // const listOfPosts = JSON.parse(xhr.response); //*
-    };
-
-    xhr.send(JSON.stringify(data));
-  });
-  return promise;
+  return fetch(url, {
+    //  method: 'GET' // as default
+    method: method,
+    //    body: JSON.stringify(data),
+    body: data, // already knows is a FormData
+    // headers: {
+    //   'Content-Type': 'application/json', // standard
+    // },
+  })
+    .then((response) => {
+      // response.text(); stream to snapshot only
+      // response.blob(); for files
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        // need to return this promise to get the proper error propagation
+        // otherwise the error is thrown here, not the outside promise chain, l.381
+        return response.json().then((errData) => {
+          console.log(errData);
+          throw new Error('Something went wrong - server side.');
+        });
+        // throw new Error('Something went wrong - server side.'); // dont work here we miss errData access
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error('Somethin went wrong!'); // This again wont work for 404 etc, only network issues
+    });
 }
 
 async function fetchPosts() {
-  const responseData = await sendHttpRequest(
-    'GET',
-    'https://jsonplaceholder.typicode.com/posts'
-  );
+  try {
+    const responseData = await sendHttpRequest(
+      'GET',
+      'https://jsonplaceholder.typicode.com/posts'
+    );
 
-  const listOfPosts = responseData;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector('h2').textContent = post.title.toUpperCase();
-    postEl.querySelector('p').textContent = post.body;
-    listElement.append(postEl);
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector('h2').textContent = post.title.toUpperCase();
+      postEl.querySelector('p').textContent = post.body;
+      postEl.querySelector('li').id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
 }
 
@@ -51,40 +187,41 @@ async function createPost(title, content) {
     userId: userId,
   };
 
-  sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+  // DEPENDS ON THE API
+  // If you dont append, js will try to collect the form data
+  // you need name="title" and name="body" in the <input> in the index.html
+  const formData = new FormData(form);
+  // formData.append('title', title);
+  // formData.append('body', content);
+  formData.append('userId', userId);
+//  formData.append('someFile', *filepath*, 'photo.png');
+
+  // sendHttpRequest('POST', 'https://jsonplaceholder.typicode.com/posts', post);
+  sendHttpRequest(
+    'POST',
+    'https://jsonplaceholder.typicode.com/posts',
+    formData
+  );
 }
 
-fetchButton.addEventListener('click', () => { // could also use () => {fetchposts();}
-  if (!listElement.hasChildNodes()) {
-    fetchPosts();
-  } else {
-    const postItem = document.querySelectorAll('.post-item');
-    for (const post of postItem) {
-      listElement.removeChild(post);
-    }
-    fetchPosts();
-  }
-});
+fetchButton.addEventListener('click', fetchPosts);
 
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // avoid broswer submitting the form
+  event.preventDefault();
   const enteredTitle = event.currentTarget.querySelector('#title').value;
   const enteredContent = event.currentTarget.querySelector('#content').value;
 
   createPost(enteredTitle, enteredContent);
 });
 
-/* Without async await
-function fetchPosts() {
-  sendHttpRequest('GET', 'https://jsonplaceholder.typicode.com/posts').then(
-    (responseData) => {
-      const listOfPosts = xhr.response; 
-      for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector('h2').textContent = post.title.toUpperCase();
-        postEl.querySelector('p').textContent = post.body;
-        listElement.append(postEl);
-      }
-    }
-  );
-*/
+// DELETE REQUEST
+
+postList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const postId = event.target.closest('li').id;
+    sendHttpRequest(
+      'DELETE',
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
