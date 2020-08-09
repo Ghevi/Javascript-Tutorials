95ca7b7b647e48673cfbff4c71320aaf96307984
Ghevi <ghevi96@gmail.com>
2020-08-09 17:47:18 +0200

    Finished browser support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+// import 'core-js/features/promise';
+
 const button = document.querySelector('button');
 const textParagraph = document.querySelector('p');
 
 button.addEventListener('click', () => {
   const text = textParagraph.textContent;
+  const promise = new Promise();
+  console.log(promise);
 
   if (navigator.clipboard) {
     navigator.clipboard
@@ -17,3 +21,17 @@ button.addEventListener('click', () => {
     alert('Feature not available, please copy manually!');
   }
 });
+
+// Solution: Polyfills
+
+// Could use polyfills package that teaches older browser to use a feature
+// by using workaround code that do the same thing.
+
+// Solution: Transpilation
+
+// For core feature of modern javascript like let, const, ()=>{}
+// There is no way to rewrite this with other tools
+// So we transpile the code, like in angular ts to js, with for example Babel
+// Babel can be integrated in the webpack workflow
+
+// Best solution: automatic detect + polyfills with Babel
